a4612cf790a0eed71280f09962f563a5fbd1ad91
Boris Dundakov <71731579+BorisDundakov@users.noreply.github.com>
2022-01-16 13:18:41 +0200

    Delete OOP_EXAM_PREP_02_APRIL_2020 directory

--- OOP_EXAM_PREP_02_APRIL_2020/skeleton/project/card/magic_card.py
+++
@@ -1,9 +0,0 @@
-from card.card import Card
-
-
-class MagicCard(Card):
-    def __init__(self, name: str, damage_points: int, health_points: int):
-        super().__init__(name, damage_points, health_points)
-        self.damage_points = 5
-        self.health_points = 80
-        self.name = name
